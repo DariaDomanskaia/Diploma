2aa22e8fa44a98736b4df79f33b53b5ff17a4508
d.domanskaya <qadomanskaia@gmail.com>
2023-12-14 20:17:48 +0300

    modified blog page

--- src/app/views/article/article-routing.module.ts
+++ src/app/views/article/article-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BlogComponent} from "./blog/blog.component";
+import {ArticlePageComponent} from "./article-page/article-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'articles', component: BlogComponent},
+  {path: 'articles/:url', component: ArticlePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
